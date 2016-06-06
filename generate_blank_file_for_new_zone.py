7d57fb61e74de75b4954ba4e152ed85a8a60555f
Stan Whitcomb <stanwhitcomb@outlook.com>
2016-06-06 15:26:44 -0700

    added the ability to add multiple meters to each site

--- generate_blank_file_for_new_zone.py
+++ generate_blank_file_for_new_zone.py
@@ -4,22 +4,37 @@
 # return a KML with folders for all ID's and a boundary
 # option to run with a list of push pin locations
 
+import re
 from kml_components import KMLCreator as kml
 from email_attach import email_myself
 
 new_kml_file = "new_kml_doc.kml"
 outf = open(new_kml_file, 'w')
-info_file = open('ids_for_new_file.txt', 'r')
+info_file = open('id_lon_lat.txt', 'r')
+#info_file = open('ids_for_new_file.txt', 'r')
 
-new_kml = kml.header("Somnoi") + kml.boundary_style() + kml.overall_folder_start("Somnoi")
+agency_name = "San Jose Water"
+new_kml = kml.header(agency_name) + kml.meter_style() + kml.boundary_style() + kml.overall_folder_start(agency_name)
 
-# info comes id,longitude,latitude in the file
+# info comes id,latitude,longitude in the file
 info = [tuple(line.split(',')) for line in info_file]
 
-for id, lon, lat, in info:
-    new_kml += kml.folder_with_pin(lon, lat.strip(), id)
-new_kml += kml.overall_folder_close()
+sites = {}
+for id, lat, lon, service_num, service_addr, water_use in info:
+    water_use = water_use.strip()
+    if sites.get(id) is None:
+        sites[id] = []
+    sites[id].append((lon, lat, service_num, service_addr, water_use))
 
+for id, meter in sites.iteritems():
+    new_kml += kml.folder_in_folder_start(id)
+    new_kml += kml.boundary_poly()
+    for lon, lat, service_num, service_addr, water_use in meter:
+        service_addr = re.sub(r'&','and', service_addr)
+        if lon != '#N/A' or lat != '#N/A':
+            new_kml += kml.meter_icon(id, lon, lat, service_num, service_addr, water_use)
+    new_kml += kml.folder_in_folder_end()
+new_kml += kml.overall_folder_close()
 
 outf.write(new_kml)
 outf.close()
--- kml_components.py
+++ kml_components.py
@@ -51,6 +51,48 @@ class KMLCreator():
     """
 
     @staticmethod
+    def meter_style():
+        return """<Style id="s_ylw-pushpin">
+        <IconStyle>
+            <scale>1.1</scale>
+            <Icon>
+                <href>http://maps.google.com/mapfiles/kml/paddle/M.png</href>
+            </Icon>
+            <hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
+        </IconStyle>
+        <ListStyle>
+            <ItemIcon>
+                <href>http://maps.google.com/mapfiles/kml/paddle/M-lv.png</href>
+            </ItemIcon>
+        </ListStyle>
+    </Style>
+    <StyleMap id="m_ylw-pushpin">
+        <Pair>
+            <key>normal</key>
+            <styleUrl>#s_ylw-pushpin</styleUrl>
+        </Pair>
+        <Pair>
+            <key>highlight</key>
+            <styleUrl>#s_ylw-pushpin_hl</styleUrl>
+        </Pair>
+    </StyleMap>
+    <Style id="s_ylw-pushpin_hl">
+        <IconStyle>
+            <scale>1.3</scale>
+            <Icon>
+                <href>http://maps.google.com/mapfiles/kml/paddle/M.png</href>
+            </Icon>
+            <hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
+        </IconStyle>
+        <ListStyle>
+            <ItemIcon>
+                <href>http://maps.google.com/mapfiles/kml/paddle/M-lv.png</href>
+            </ItemIcon>
+        </ListStyle>
+    </Style>
+    """
+        
+    @staticmethod
     def boundary_style():
         return """<Style id="boundaryPoly12201701227625140102030021700121013111910600210512110">
         <LineStyle>
@@ -80,14 +122,68 @@ class KMLCreator():
             <styleUrl>#boundaryPoly12331321942161700510000212103521017126125045003121460</styleUrl>
         </Pair>
     </StyleMap>
-        """
+    """
 
     @staticmethod
     def overall_folder_start(name):
         return """<Folder>
         <name>%s</name>
         <open>1</open>
-    """ % (name)
+        """ % (name)
+
+    @staticmethod
+    def folder_in_folder_start(name):
+        return """<Folder>
+            <name>%s</name>
+            <open>1</open>
+            """ % (name)
+
+    @staticmethod
+    def folder_in_folder_end():
+        return """</Folder>
+        """
+
+    @staticmethod
+    def boundary_poly():
+        return """<Placemark>
+                <name>Boundary</name>
+                <styleUrl>#boundaryPoly1210666825101131200012000010423950104012628108200220023653</styleUrl>
+                <Polygon>
+                    <tessellate>1</tessellate>
+                    <outerBoundaryIs>
+                        <LinearRing>
+                            <coordinates>
+                            </coordinates>
+                        </LinearRing>
+                    </outerBoundaryIs>
+                </Polygon>
+            </Placemark>
+        """
+
+    @staticmethod
+    def meter_icon(id, lon, lat, service_num, service_addr, water_use):
+        return """<Placemark>
+                <name>%s</name>
+                <description>Service Number: %s
+Service Address: %s
+Water use: %s
+                </description>
+                <LookAt>
+                    <longitude>%s</longitude>
+                    <latitude>%s</latitude>
+                    <altitude>0</altitude>
+                    <heading>0</heading>
+                    <tilt>0</tilt>
+                    <range>1000</range>
+                    <gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
+                </LookAt>
+                <styleUrl>#m_ylw-pushpin</styleUrl>
+                <Point>
+                    <gx:drawOrder>1</gx:drawOrder>
+                    <coordinates>%s,%s,0</coordinates>
+                </Point>
+            </Placemark>
+            """ % (id, service_num, service_addr, water_use, lon, lat, lon, lat)
 
     @staticmethod
     def folder_no_pin(id):
